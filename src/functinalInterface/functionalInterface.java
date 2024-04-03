6e25dee12c3078720adb43f930e69581014b097c
Habib-hs <mhabibullahhs@gmail.com>
2024-04-04 00:06:17 +0600

    first commit

---
+++ src/functinalInterface/functionalInterface.java
@@ -0,0 +1,8 @@
+package functinalInterface;
+
+public class functionalInterface {
+    public static void main(String[] args) {
+        System.out.println("hello functional Interface");
+    }
+
+}
